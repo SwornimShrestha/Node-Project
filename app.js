56fc96b48e4c3a5328c35807c5b1727045c07c7c
Swornim Shrestha <swornimshrestha639@gmail.com>
2024-08-24 19:34:58 +0545

    createOperation

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
-const express = require("express");
-const app = express();
 require("dotenv").config();
+const express = require("express");
+const { blogs } = require("./model/index");
+const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("public/css"));
+
 require("./model/index");
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.render("home.ejs");
 });
 
-app.get("/about", (req, res) => {
-  res.send("About Us Page");
+app.get("/create", (req, res) => {
+  res.render("create.ejs");
 });
 
+app.post("/create", async (req, res) => {
+  console.log(req.body);
+  const { title, subtitle, description } = req.body;
+  await blogs.create({
+    title,
+    subtitle,
+    description,
+  });
+  res.send("Blog added Successfully");
+});
 app.listen(3000, () => {
   console.log("Project Start");
 });
